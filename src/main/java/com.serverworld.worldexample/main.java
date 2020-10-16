687608bcbeb7f503233afad775100d37931d5761
terry <>
2020-10-16 15:25:05 +0800

    .

--- src/main/java/com.serverworld.worldexample/main.java
+++ src/main/java/com.serverworld.worldexample/main.java
@@ -1,7 +1,21 @@
 package com.serverworld.worldexample;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class main {
-    public static void main(String[] args){
+
+    public static void main(String[] args)  {
+        Sqlcon X = new Sqlcon();
+        X.MYSQLlogin("127.0.0.1","database","testuser","v4GtTyH1ws9RFZ1r",3306);
+        try {
+            //Statement statement = Sqlcon.getConnection().createStatement();
+       }catch (Exception e){
+            e.printStackTrace();
+        }
+
 
     }
+
+
 }
